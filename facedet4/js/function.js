a00f26cc4b64301e0691c0b956327d3c6ca19ee7
Ilse <ilse@ccdigital.mx>
2014-04-07 17:10:47 -0500

    Variante que permite manejar 3 tipos de imágenes (sombrero, cara, imagen fija) para IE & el resto.

--- facedet4/js/function.js
+++ facedet4/js/function.js
@@ -1,101 +1,161 @@
 function cambiarImagen(numImg){
     var imagen = numImg;
-    var ruta;
+    var ruta, tipoImg;
     switch(imagen){
         case '1':
             ruta = "facedet/glasses_original.png";
+            tipoImg = "cara";
             break;
-    case '2':
+        case '2':
             ruta = "facedet/glasses2.png";
+            tipoImg = "cara";
             break;
-    case '3':
+        case '3':
             ruta = "facedet/mono.png";
+            tipoImg = "cara";
             break;
-    case '4':
-            ruta = "facedet/ojos.png";
-            break;
-    case '5':
+        case '5':
             ruta = "facedet/hat.png";
+            tipoImg = "sombrero";
+            break;
+        case '6':
+            ruta = "facedet/rayos.png";
+            tipoImg = "imagen";
             break;
     }
-    window.cancelAnimationFrame(drawToCanvas);
-    window.cancelAnimationFrame(drawToCanvasIE);
     document.getElementById("elegida").innerHTML = ruta;
-    
+    stopAllAnimations();
     if (navigator.appName == "Microsoft Internet Explorer") {
         drawToCanvasIE();
+         switch(tipoImg){
+            case "cara":
+                drawFaceIE();
+                break;
+            case "sombrero":
+                drawHatIE();
+                break;
+            case "imagen":
+                drawFixedImage();
+                break;
+        }
     } else {
-        drawToCanvas();
-    }
-}
-
-var tipoImg; 
-
-function setTipoImg(tipo){
-    tipoImg = tipo;
-}
-
-function drawToCanvas(){
-    window.requestAnimationFrame(drawToCanvas);
-    var c=document.getElementById("canvas");
-    var ctx=c.getContext("2d");
-
-    var comp = ccv.detect_objects({
-        canvas: c,
-        cascade: cascade,
-        interval: 1, 
-        min_neighbors: 1
-    });
-
-    var img = new Image();
-    var rutaImgn = document.getElementById("elegida").innerHTML;
-    if (rutaImgn == "Elige una imagen"){
-        img.src = "facedet/glasses_original.png";
-    } else {
-        img.src = document.getElementById("elegida").innerHTML;
-        //img.src = "facedet/hat.png";
-    }
-    /*switch(rutaImgn){
-    case 'facedet/hat.png': //Tipo sombrero
-
-            break;
-    case '2': // Tipo cara
-            ruta = "facedet/glasses2.png";
-            break;
-    } */
-    if (comp.length == 0){
-        //ctx.drawImage(img, 100, 40, 150, 150); //IE
-    } else {
-        for (i = comp.length; i--; ) {
-            ctx.drawImage(img, comp[i].x, comp[i].y, comp[i].width, comp[i].height);
-            //ctx.drawImage(img, comp[i].x, (comp[i].y - comp[i].height), comp[i].width, comp[i].height);
+        switch(tipoImg){
+            case "cara":
+                drawFace();
+                break;
+            case "sombrero":
+                drawHat();
+                break;
+            case "imagen":
+                drawFixedImage();
+                break;
         }
     }
 }
 
-function drawToCanvasIE(){
-    window.requestAnimationFrame(drawToCanvas);
+function stopAllAnimations(){
+    window.cancelAnimationFrame(drawFace);
+    window.cancelAnimationFrame(drawHat);
+    window.cancelAnimationFrame(drawFixedImage);
+    window.cancelAnimationFrame(drawFaceIE);
+    window.cancelAnimationFrame(drawHatIE);
+    console.log("Stop");
+}
+
+function drawFace(){
+    console.log("face");
+    window.requestAnimationFrame(drawFace);
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+
+    var comp = ccv.detect_objects({
+        canvas: c,
+        cascade: cascade,
+        interval: 1,
+        min_neighbors: 1
+    });
+
+    var img = new Image();
+    img.src = document.getElementById("elegida").innerHTML;
+
+    for (i = comp.length; i--; ) {
+        ctx.drawImage(img, comp[i].x, comp[i].y, comp[i].width, comp[i].height);
+    }
+    
+}
+
+function drawHat(){
+    console.log("hat");
+    window.requestAnimationFrame(drawHat);
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+
+    var comp = ccv.detect_objects({
+        canvas: c,
+        cascade: cascade,
+        interval: 1,
+        min_neighbors: 1
+    });
+
+    var img = new Image();
+    img.src = document.getElementById("elegida").innerHTML;
+
+    for (i = comp.length; i--; ) {
+        ctx.drawImage(img, comp[i].x, (comp[i].y - (comp[i].height * 0.9)), comp[i].width, comp[i].height);
+    }
+}
+
+function drawFixedImage(){
+    console.log("image");
+    //window.requestAnimationFrame(drawFixedImage);
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.src = document.getElementById("elegida").innerHTML;
+    ctx.drawImage(img, 0, 0, c.width, c.height);
+}
+
+function drawFaceIE(){
+    window.requestAnimationFrame(drawFaceIE);
     var c=document.getElementById("canvas");
     var ctx=c.getContext("2d");
 
     var comp = ccv.detect_objects({
         canvas: c,
         cascade: cascade,
-        interval: 1, 
+        interval: 1,
         min_neighbors: 1
     });
 
     var img = new Image();
-    var rutaImgn = document.getElementById("elegida").innerHTML;
-    if (rutaImgn == "Elige una imagen"){
-        img.src = "facedet/glasses_original.png";
-    } else {
-        img.src = document.getElementById("elegida").innerHTML;
-    }
-                
+    img.src = document.getElementById("elegida").innerHTML;
+
     if (comp.length == 0){
-        document.getElementById('imagenie').style.display = "inline";
-        document.getElementById('imagenie').src = rutaImgn;
+        ctx.drawImage(img, 105, 50, 160, 160);
+    } else {
+        for (i = comp.length; i--; ) {
+            ctx.drawImage(img, comp[i].x, comp[i].y, comp[i].width, comp[i].height);
+        }
+    }
+}
+
+function drawHatIE(){
+    window.requestAnimationFrame(drawHatIE);
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+
+    var comp = ccv.detect_objects({
+        canvas: c,
+        cascade: cascade,
+        interval: 1,
+        min_neighbors: 1
+    });
+
+    var img = new Image();
+    img.src = document.getElementById("elegida").innerHTML;
+
+    if (comp.length == 0){
+        ctx.drawImage(img, 120, 0, 130, 120);
     } else {
         for (i = comp.length; i--; ) {
             ctx.drawImage(img, comp[i].x, comp[i].y, comp[i].width, comp[i].height);
